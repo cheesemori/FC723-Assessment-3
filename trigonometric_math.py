c162f1734749e71df2fb4c4e793149185e2703fb
abdullah malallah <amalallah14@gmail.com>
2025-02-19 14:24:18 +0000

    updated function

--- trigonometric_math.py
+++ trigonometric_math.py
@@ -3,3 +3,28 @@ def cal_sin(num):
     radian = math.radians(num)
     result = math.sin(radian)
     return result
+
+def cal_cos(num):
+    radian = math.radians(num)
+    result = math.cos(radian)
+    return result
+
+def cal_tan(num):
+    radian = math.radians(num)
+    result = math.tan(radian)
+    return result
+
+def cal_asin(num):
+    radian = math.asin(num)
+    result = math.degrees(radian)
+    return result
+
+def cal_acos(num):
+    radian = math.acos(num)
+    result = math.degrees(radian)
+    return result
+
+def cal_atan(num):
+    radian = math.atan(num)
+    result = math.degrees(radian)
+    return result
