bcaa6c918940af57fe8193e7bf59ee4014f9244c
cheesemori <2274159818@qq.com>
2025-02-19 14:19:47 +0000

    update main code

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import tkinter as tk
-
+import trigonometric_math as tm
 
 main_window = tk.Tk()
 main_window.geometry('400x600')
 entry_var = tk.StringVar()
-label_var = tk.StringVar()
 screen_entry = tk.Entry(main_window,textvariable=entry_var,bd=2,relief="ridge",font=("Arial", 20))
 screen_entry.pack(fill="both",padx=5,pady=5)
 main_window.columnconfigure(0, weight=1)
@@ -26,26 +25,29 @@ text_lst = []
 
 def button_input(button_text):
     current_text = entry_var.get()
-    if button_text in ["sin","cos","tan","sqrt","asin","acos","atan","square"]:
+    if button_text in ["+","-","*","/"]:
+            entry_var.set(current_text + button_text)
+            text_lst.append(current_text + button_text)
+    elif button_text in ["sin","cos","tan","sqrt","asin","acos","atan","square"]:
         num = float(current_text)
         if button_text == "sin":
-            pass
+            show_result(tm.cal_sin(num))
         elif button_text == "cos":
-            pass
+            show_result(tm.cal_cos(num))
         elif button_text == "tan":
-            pass
+            show_result(tm.cal_tan(num))
         elif button_text == "sqrt":
             show_result(num**0.5)
         elif button_text == "asin":
-            pass
+            show_result(tm.cal_asin(num))
         elif button_text == "acos":
-            pass
+            show_result(tm.cal_acos(num))
         elif button_text == "atan":
-            pass
+            show_result(tm.cal_atan(num))
         elif button_text == "square":
             show_result(num**2)
     elif button_text == "=":
-        show_result(entry_var.get())
+        show_result(str(eval(current_text)))
     elif button_text == "C":
         entry_var.set("")
         show_result("")
@@ -63,7 +65,7 @@ def button_input(button_text):
 
 
 def show_result(result):
-    label_var.set(f"{result}")
+    entry_var.set(result)
 
 for button_row in buttons:
     row_frame = tk.Frame(button_frame)
@@ -71,6 +73,6 @@ for button_row in buttons:
     for button in button_row:
         tk.Button(row_frame,text=button,font=("Arial", 16),command=lambda b=button:button_input(b)).pack(side="left",fill="both",expand=True)
 
-result_label = tk.Label(main_window,font=("Arial", 16),bd=2, relief="ridge",textvariable=label_var).pack()
+
 if __name__ == '__main__':
     main_window.mainloop()
